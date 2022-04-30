23823af056a4c1146ca2a53097e6aafde0b69936
Darron Brown <dabrown18@live.com>
2022-04-29 20:26:38 -0600

    Persisting authentication

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import { MainStack, AuthStack } from './src/navigation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
-  return (
-    <AuthStack />
-  );
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  const getToken = async () => {
+    try {
+      const value = await AsyncStorage.getItem('token');
+      if (value !== null) {
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      // Error retrieving data
+      setIsAuthenticated(false);
+    }
+  };
+
+  return isAuthenticated ? <MainStack /> : <AuthStack />;
 };
 
 export default App;
--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,12 +1,29 @@
 import React, {useState} from 'react';
+import {Alert} from 'react-native';
 import LoginView from './LoginView';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   const [username, onChangeUsername] = useState('');
   const [password, onChangePassword] = useState('');
 
-  const onPressLogin = () => {
-
+  const onPressLogin = async () => {
+    try {
+      console.log('hello there');
+      await AsyncStorage.setItem(
+        'token',
+        '12345'
+      );
+    } catch (error) {
+      // Error saving data
+      Alert.alert(
+        'Login failed',
+        `${error}`,
+        [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ]
+      );
+    }
   };
 
   return (
